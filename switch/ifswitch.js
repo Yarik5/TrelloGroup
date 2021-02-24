5628260883e1a70023152b60d6b1edffc66255b3
Dogi <yarikgnatuk78@gmail.com>
2021-02-24 21:50:09 +0200

    operator switch

---
+++ switch/ifswitch.js
@@ -0,0 +1,11 @@
+if (browser == 'Edge') {
+     console.log( "You've got the Edge!" );
+} else if (browser == 'Chrome' 
+|| browser == 'Firefox' 
+|| browser == 'Safari' 
+|| browser == 'Opera'); {
+console.log( 'Okey we wupport where browsers too' ) 
+} else {
+    
+console.log( 'We hope that this page looks ok!' );
+}
