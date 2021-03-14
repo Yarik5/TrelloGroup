bc6fca8a674285b976b3f79ff668622fa8ecce06
Dogi <yarikgnatuk78@gmail.com>
2021-03-14 15:10:35 +0200

    correct

--- switch/ifswitch.js
+++ switch/ifswitch.js
@@ -1,11 +1,12 @@
-if (browser == 'Edge') {
-     console.log( "You've got the Edge!" );
+const browser = prompt('What your browser?')
+
+if(browser == 'Edge') {
+     alert( "You've got the Edge!" );
 } else if (browser == 'Chrome' 
 || browser == 'Firefox' 
 || browser == 'Safari' 
 || browser == 'Opera') {
-console.log( 'Okey we wupport where browsers too' ) 
+     alert( 'Okey we support these browsers too' );
 } else {
-
-console.log( 'We hope that this page looks ok!' );
+     alert( 'We hope that this page looks ok!');
 }
